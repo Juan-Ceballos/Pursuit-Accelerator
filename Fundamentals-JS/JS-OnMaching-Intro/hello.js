70aaa3c1ddb02740d759ce924ebeeabfa19dcf23
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-01-22 20:56:35 -0500

    extra comments for how argvs works

--- Fundamentals-JS/JS-OnMaching-Intro/hello.js
+++ Fundamentals-JS/JS-OnMaching-Intro/hello.js
@@ -1,7 +1,15 @@
 console.log('Hello, Node.js!');
 function sayHello(name, lName) {
     let result = "HELLO!";
-    if (name, lName) {
+    if (name && lName) {
+        return `Hello, ${name} ${lName}!`;
+    }
+    return result;
+}
+
+function sayHello2(name, lName) {
+    let result = "HELLO!";
+    if (name, lName) { // comma operator evaluates left ot right, returns value of last operand
         return `Hello, ${name} ${lName}!`;
     }
     return result;
@@ -9,5 +17,9 @@ function sayHello(name, lName) {
 
 const nameInput = process.argv[2];
 const lNameInput = process.argv[3];
+// process array holds key values for info on what nodejs is running
+// the argv key has an array of the arguments being ran in the node command
+// the first and second is the path to the node program and the .js file
+// 0 - node, 1 - hello.js, the third and after is every argument entered after that
 const result = sayHello(nameInput, lNameInput);
 console.log(result);
